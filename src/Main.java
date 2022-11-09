6c25ce8cf1c659974de36b06c1e8175f59f077a4
Tedros Kiflom <tedroskiflom@gmail.com>
2022-11-09 13:23:20 +0100

    Checkpoint new feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tedros första checkpoint");
+        System.out.println("Tedros första checkpoint. New feature.");
     }
 }
